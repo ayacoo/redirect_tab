e354a80f39f6cc7aa1c61abb0260f3f3ffa41392
Guido Schmechel <guido@ayacoo.de>
2024-05-01 17:53:44 +0200

    [TASK] Add quality stage

--- Tests/Functional/Service/RedirectDemandServiceTest.php
+++ Tests/Functional/Service/RedirectDemandServiceTest.php
@@ -8,6 +8,7 @@ use Ayacoo\RedirectTab\Service\RedirectDemandService;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use TYPO3\CMS\Core\Configuration\Event\SiteConfigurationBeforeWriteEvent;
 use TYPO3\CMS\Core\Configuration\SiteConfiguration;
+use TYPO3\CMS\Core\Site\Entity\NullSite;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
@@ -31,7 +32,21 @@ final class RedirectDemandServiceTest extends FunctionalTestCase
      */
     public function getRedirectsReturnsPerDefaultAnEmptyArray(): void
     {
-        $result = $this->subject->getRedirects(1);
+        $result = $this->subject->getRedirects();
+
+        self::assertCount(0, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function getRedirectsWithNullSiteReturnsEmptyArray(): void
+    {
+        $site = new NullSite();
+        $data = [];
+        $data['site'] = $site;
+        $this->subject->setData($data);
+        $result = $this->subject->getRedirects();
 
         self::assertCount(0, $result);
     }
@@ -78,5 +93,49 @@ final class RedirectDemandServiceTest extends FunctionalTestCase
         $result = $this->subject->getRedirects(1);
 
         self::assertCount(1, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function getRedirectsWithMultiDomainsReturnsArray(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/pages.csv');
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/sys_redirect_with_multi_domains.csv');
+
+        $identifier = 'website-local';
+        $configuration = [
+            'rootPageId' => 1,
+            'base' => 'http://example.com/',
+        ];
+
+
+        $event = new SiteConfigurationBeforeWriteEvent($identifier, $configuration);
+        $eventDispatcherMock = $this->getMockBuilder(EventDispatcherInterface::class)->getMock();
+        $eventDispatcherMock->expects(self::once())->method('dispatch')->with(self::anything())->willReturn($event);
+
+        $siteConfiguration = new SiteConfiguration(
+            $this->instancePath . '/typo3conf/sites/',
+            $eventDispatcherMock
+        );
+
+        try {
+            $siteConfiguration->write($identifier, $configuration);
+        } catch (\Exception $exception) {
+            self::markTestSkipped($exception->getMessage());
+        }
+
+        $siteFinder = new SiteFinder();
+        $site = $siteFinder->getSiteByPageId(1);
+
+        $data = [];
+        $data['site'] = $site;
+        $data['databaseRow']['sys_language_uid'] = 0;
+        $data['effectivePid'] = 1;
+
+        $this->subject->setData($data);
+        $result = $this->subject->getRedirects(1);
+
+        self::assertCount(2, $result);
     }
 }
