058dfaf0e150b7cef15ab3d1063a5bab7ccbf90d
Guido Schmechel <guido@ayacoo.de>
2025-12-24 15:37:18 +0100

    Update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@ayacoo.de',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '4.0.2',
+    'version' => '4.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '13.0.0-13.4.99',
             'redirects' => '13.0.0-13.4.99',
-            'php' => '8.2.0-8.4.99',
+            'php' => '8.2.0-8.5.99',
         ],
         'conflicts' => [
         ],
